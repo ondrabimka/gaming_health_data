93864b5853331ec998b967888d90e831f5be08c8
ondrabimka <ondrabimka@seznam.cz>
2025-02-23 15:04:07 +0100

    updated dualsense L2 R2 buttons

--- gaming_health_data/data_loggers/PS/dualsense.py
+++ gaming_health_data/data_loggers/PS/dualsense.py
@@ -34,15 +34,15 @@ with open(filename, "w", newline="") as file:
                 if joystick.get_button(i):
                     writer.writerow([timestamp, f"Button {i}", 1])
                     print(f"{timestamp}, Button {i}, Pressed")
-
-            # Log joystick movements (ignore small drift < 0.1)
+            # Log joystick movements (ignore small drift < 0.1) and ignore axis 4 and 5 (l2 and r2) if they are not pressed (-1.0)
             for i in range(joystick.get_numaxes()):
                 axis_value = joystick.get_axis(i)
-                if abs(axis_value) > 0.1:  # Ignore small movements
+                if i in [4, 5] and axis_value == -1.0:
+                    continue
+                if abs(axis_value) > 0.1:
                     writer.writerow([timestamp, f"Axis {i}", round(axis_value, 3)])
                     print(f"{timestamp}, Axis {i}, {round(axis_value, 3)}")
-
-            time.sleep(0.01)  # Prevent excessive CPU usage
+            time.sleep(0.001)  # Prevent excessive CPU usage
 
     except KeyboardInterrupt:
         print("\nLogging stopped. CSV file saved.")
